58e11b9c013c7a6622a609bba5b944389cd28af6
Adnan Macanovic Omerbasic <adna0180@edu.zealand.dk>
2023-12-07 12:48:56 +0100

    Add project files.

---
+++ RoomVitalLib/Model/Bruger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomVitalLib.Model
+{
+    public class Bruger
+    {
+
+        public string BrugerNavn { get; set; }
+        public string Password { get; set; }
+
+
+        public Bruger(string brugerNavn, string password)
+        {
+            BrugerNavn = brugerNavn;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(BrugerNavn)}={BrugerNavn}, {nameof(Password)}={Password}}}";
+        }
+    }
+}
---
+++ RoomVitalLib/Model/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomVitalLib.Model
+{
+    public class Sensor
+    {
+        public int Temperatur { get; set; }
+        public int Luftfugtighed { get; set; }
+        public int Luftkvalitet { get; set; }
+
+        public Sensor(int temperatur, int luftfugtighed, int luftkvalitet)
+        {
+            Temperatur = temperatur;
+            Luftfugtighed = luftfugtighed;
+            Luftkvalitet = luftkvalitet;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Temperatur)}={Temperatur.ToString()}, {nameof(Luftfugtighed)}={Luftfugtighed.ToString()}, {nameof(Luftkvalitet)}={Luftkvalitet.ToString()}}}";
+        }
+    }
+}
